070ee84d68ba2dab32742e3489975892a7674818
heidenreich20 <heidenreich20@hotmail.com>
2023-09-04 17:51:14 -0300

    minor fixes

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,12 @@
-const ReviewCard = ({ score, profile, review }) => {
+const ReviewCard = ({ score, profile, review, name }) => {
   return (
     <article className='flex p-5 font-inter bg-neutral-100 rounded-lg gap-3 flex-row md:flex-col justify-center items-center md:max-w-[250px] max-w-[95%] w-full'>
       <div className='flex justify-center flex-col items-center gap-3'>
         <img className='rounded-full' src={profile} alt='reviewer profile' />
-        <h3 className='font-bold'>Opinión: {score}</h3>
+        <p className='font-bold text-neutral-700'>{name}</p>
+        <h3 className='font-bold text-neutral-700'>Opinión: {score}</h3>
       </div>
-      <blockquote className='italic'>
+      <blockquote className='italic text-neutral-600'>
         <p>{review}</p>
       </blockquote>
     </article>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,12 +8,12 @@ import user4 from '../assets/user4.jpg'
 const Testimonials = () => {
   return (
     <section className='flex md:mx-0 mx-5 items-center flex-col w-full mb-20'>
-     <h2 className='flex items-center outline mb-20 outline-2 p-1 px-4 rounded-full justify-center text-sm md:text-xl font-bold font-inter text-sky-700'><img className='flex aspect-auto w-6 md:w-8' src={scoreIcon} />Testimonios</h2>
+      <h2 className='flex items-center outline mb-20 outline-2 p-1 px-4 rounded-full justify-center text-sm md:text-xl font-bold font-inter text-sky-700'><img className='flex aspect-auto w-6 md:w-8' src={scoreIcon} />Testimonios</h2>
       <div className='costumGrid justify-items-center sm:w-2/3 w-full text-center'>
-        <ReviewCard profile={user1} score='⭐⭐⭐⭐⭐' review='Lorem ipsum dolor sit amet consectetur, adipisicing elit. At incidunt eligendi error ducimus officia quibusdam dolores cum tempore nobis facilis. Magni dolorum deleniti pariatur ad doloremque, obcaecati itaque saepe aut?' />
-        <ReviewCard profile={user2} score='⭐⭐⭐⭐⭐' review='Lorem ipsum dolor sit amet consectetur, adipisicing elit. At incidunt eligendi error ducimus officia quibusdam dolores cum tempore nobis facilis. Magni dolorum deleniti pariatur ad doloremque, obcaecati itaque saepe aut?' />
-        <ReviewCard profile={user3} score='⭐⭐⭐⭐' review='Lorem ipsum dolor sit amet consectetur, adipisicing elit. At incidunt eligendi error ducimus officia quibusdam dolores cum tempore nobis facilis. Magni dolorum deleniti pariatur ad doloremque, obcaecati itaque saepe aut?' />
-        <ReviewCard profile={user4} score='⭐⭐⭐⭐' review='Lorem ipsum dolor sit amet consectetur, adipisicing elit. At incidunt eligendi error ducimus officia quibusdam dolores cum tempore nobis facilis. Magni dolorum deleniti pariatur ad doloremque, obcaecati itaque saepe aut?' />
+        <ReviewCard profile={user1} name='Juan Martinez' score='⭐⭐⭐⭐⭐' review='Fui a FakeDental para un chequeo de rutina y quedé impresionado. La limpieza y el cuidado que brindaron a mis dientes fue excepcional. Definitivamente seguiré viniendo aquí para mis necesidades dentales.' />
+        <ReviewCard profile={user2} name='Maria Rodriguez' score='⭐⭐⭐⭐⭐' review='Excelente servicio en FakeDental. El personal es amable y profesional. Mi visita al dentista fue sin dolor y muy informativa. ¡Recomendaría FakeDental a todos mis amigos y familiares!' />
+        <ReviewCard profile={user3} name='Ana Lopez' score='⭐⭐⭐⭐' review='FakeDental ha sido mi elección de dentista durante años, y nunca me ha decepcionado. El equipo siempre es atento y se toman el tiempo para explicar cualquier procedimiento. ' />
+        <ReviewCard profile={user4} name='Carlos Ramirez' score='⭐⭐⭐⭐' review='Me encanta FakeDental. El ambiente es relajante, y el personal es muy profesional. Recientemente tuve un blanqueamiento dental aquí, y los resultados fueron sorprendentes. ¡Mi sonrisa nunca ha lucido mejor!' />
       </div>
     </section>
   )
