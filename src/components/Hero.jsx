a05261b6955b044f0651fa70bdbd21081285551e
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-10-02 07:08:40 -0300

    added accessibility features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
         <div className='flex gap-12 flex-col h-full md:w-fit w-full items-center absolute md:translate-x-48 md:items-start justify-center'>
           <h1 className='font-inter text-sky-900 font-bold text-4xl lg:text-8xl'>¡Atrévete a <p className='text-white'>volver a sonreír!</p></h1>
           <section className='flex text-white gap-6 font-inter'>
-            <a href='#specialties' className='bg-sky-800 hover:bg-sky-900 transition-colors duration-150 ease-in-out cursor-pointer p-4 px-6 rounded-md'>Especialidades</a>
-            <a className='bg-slate-700 hover:bg-slate-800 transition-colors duration-150 ease-in-out cursor-pointer p-4 px-6 rounded-md'>Contacto</a>
+            <a href='#specialties' className='bg-sky-800 hover:bg-sky-900 transition-colors duration-150 ease-in-out cursor-pointer p-4 px-6 focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4'>Especialidades</a>
+            <a className='bg-slate-700 hover:bg-slate-800 transition-colors duration-150 ease-in-out cursor-pointer p-4 px-6 focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4'>Contacto</a>
           </section>
         </div>
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,22 +20,22 @@ const Nav = () => {
         </a>
         <ul className='font-inter md:flex hidden text-base xl:text-lg text-neutral-600 cursor-pointer gap-5 lg:gap-10'>
           <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'>Contacto</li>
-          <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a href='#aboutUs'>Quienes somos</a></li>
-          <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a href='#specialties'>Especialidades</a></li>
+          <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a className='focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4' href='#aboutUs'>Quienes somos</a></li>
+          <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a className='focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4' href='#specialties'>Especialidades</a></li>
         </ul>
         <button onClick={toggleMenu}>
           <svg className='md:hidden block stroke-sky-700' width='64px' height='64px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path d='M4 6H20M4 12H20M4 18H20' stroke='inherit' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
           </svg>
         </button>
-        <a href='#contactForm' className='text-neutral-100 md:block hidden box-content font-semibold bg-sky-700 p-2 xl:p-3 px-6 xl:px-12 rounded-md'>Pide tu cita</a>
+        <a href='#contactForm' className='text-neutral-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-sky-700 outline-offset-4 md:block hidden box-content font-semibold bg-sky-700 p-2 xl:p-3 px-6 xl:px-12 rounded-md'>Pide tu cita</a>
       </div>
       <div className={`burger-menu h-[100vh] bg-black/[0.5]  ${isMenuOpen ? 'burger-menu-open' : 'burger-menu-close'}`}>
         <div className='h-fit border-t-2 bg-neutral-50/[0.95] w-full border-sky-600'>
           <ul className='font-inter w-full justify-around flex p-5 text-base xl:text-lg text-neutral-600 cursor-pointer'>
             <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'>Contacto</li>
-            <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a href='#aboutUs'>Quienes somos</a></li>
-            <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a href='#specialties'>Especialidades</a></li>
+            <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a className='focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4' href='#aboutUs'>Quienes somos</a></li>
+            <li className='transition-all duration-150 ease-linear hover:underline hover:text-sky-600'><a className='focus-visible:outline focus-visible:outline-2 rounded-lg focus-visible:outline-sky-700 outline-offset-4' href='#specialties'>Especialidades</a></li>
           </ul>
         </div>
       </div>
